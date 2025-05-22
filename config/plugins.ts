51c7a61056944efe26434712a521f8cfaa791bed
Kaloyan Angelov <64629762+kaloyaqn@users.noreply.github.com>
2025-05-22 16:45:57 +0300

    Refactor email configuration and update Strapi Algolia plugin settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,33 +1,38 @@
-import { env } from '@strapi/utils';
+import { env } from "@strapi/utils";
 
 export default () => ({
-    email: {
-        config: {
-          provider: 'nodemailer',
-          providerOptions: {
-            host: env('SMTP_HOST'),
-            port: env('SMTP_PORT'),
-            auth: {
-              user: env('SMTP_USER'),
-              pass: env('SMTP_PASS'),
-            },
-            // ... any custom nodemailer options
-          },
-          settings: {
-            defaultFrom: env('SMTP_DEFAULT_FROM'),
-            defaultReplyTo: env('SMTP_DEFAULT_REPLYTO'),
-          },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USER"),
+          pass: env("SMTP_PASS"),
         },
+        // ... any custom nodemailer options
       },
-'strapi-algolia': {
+      settings: {
+        defaultFrom: env("SMTP_DEFAULT_FROM"),
+        defaultReplyTo: env("SMTP_DEFAULT_REPLYTO"),
+      },
+    },
+  },
+  "strapi-algolia": {
     enabled: true,
     config: {
-      apiKey: env('ALGOLIA_ADMIN_KEY'),
-      applicationId: env('ALGOLIA_APP_ID'),
+      apiKey: env("ALGOLIA_ADMIN_KEY"),
+      applicationId: env("ALGOLIA_APP_ID"),
       contentTypes: [
-        { name: 'api::article.article' },
+        { name: "api::article.article" },
         // ...
       ],
+    },
+  },
+  graphql: {
+    config: {
+      playgroundAlways: true,
     },
   },
 });
